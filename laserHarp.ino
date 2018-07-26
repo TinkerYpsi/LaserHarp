bd768d0f409f7b262d3bf6c8fb38e2c3b601f683
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-07-26 18:16:38 -0400

    Initial commit

---
+++ laserHarp.ino
@@ -0,0 +1,77 @@
+int speakerPin = 3;
+int duration = 100;
+
+int laser1 = A1;
+int laser2 = A2;
+int laser3 = A3;
+int laser4 = A4;
+int laser5 = A5;
+int laser6 = A6;
+int laser7 = A7;
+int laser8 = A8;
+int *trigLaser;
+
+int *val1;
+int *val2;
+int *val3;
+int *val4;
+int *val5;
+int *val6;
+int *val7;
+int *val8;
+int *allVals[8];
+
+
+int tone1 = NOTE_C4;
+int tone2 = NOTE_D4;
+int tone3 = NOTE_E4;
+int tone4 = NOTE_G4;
+int tone5 = NOTE_A4;
+int tone6 = NOTE_B4;
+int tone7 = NOTE_D5;
+int tone8 = NOTE_F5;
+int allTones[8] = [tone1, tone2, tone3, tone4, tone5, tone6, tone7, tone8];
+
+int* min(int *allVals[]);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  val1 = analogRead(laser1);
+  val2 = analogRead(laser2);
+  val3 = analogRead(laser3);
+  val4 = analogRead(laser4);
+  val5 = analogRead(laser5);
+  val6 = analogRead(laser6);
+  val7 = analogRead(laser7);
+  val8 = analogRead(laser8);
+  allVals = [val1, val2, val3, val4, val5, val6, val7, val8];
+  trigLaser = analogRead(allVals);
+
+  for(int i = 0; i < 8; i++)
+  {
+    if(*trigLaser == allLasers[i])
+    {
+      tone(speakerPin, allTones[i], duration);
+    }
+  }
+}
+
+int* min(int *allVals[])
+{
+  int lowestVal = 1023;
+  int *lowestLaser;
+  for(int i = 0; i < 8; i++)
+  {
+    if(*allVals[i] < lowestVal)
+    {
+      lowestVal = *allVals[i];
+      lowestLaser = allVals[i];
+    }
+  }
+  return lowestLaser;
+}
